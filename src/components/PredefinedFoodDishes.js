b26f1e077281b56f6a8092a3cc70b3566cf96078
Paolo <paolocordovacontacto@gmail.com>
2022-07-14 16:23:10 -0400

    The necessary file has been created

---
+++ src/components/PredefinedFoodDishes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PredefinedFoodDishes = () => {
+  return (
+    <div>PredefinedFoodDishes</div>
+  )
+}
+
+export default PredefinedFoodDishes;
---
+++ src/components/SearchFoodDishes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchFoodDishes = () => {
+  return (
+    <div>SearchFoodDishes</div>
+  )
+}
+
+export default SearchFoodDishes;
